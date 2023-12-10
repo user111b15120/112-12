5e1594f585dae47d3e5eb5146bb5b7dd3d6229ac
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-10 16:14:51 +0800

    Create Ex18.java

---
+++ Ex18/Ex18.java
@@ -0,0 +1,9 @@
+package L12.Ex18;
+
+public class Ex18 {
+    public static void main(String[] args){
+        String str = "45874356676541331";
+        long l = Long.parseLong(str);
+        System.out.println(l);
+    }
+}
