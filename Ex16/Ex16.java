d068f0de591c654ca06454e167f39a54f8c569ae
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-10 16:10:22 +0800

    Create Ex16.java

---
+++ Ex16/Ex16.java
@@ -0,0 +1,9 @@
+package L12.Ex16;
+
+public class Ex16 {
+    public static void main(String[] args){
+        String str = "262904713";
+        int i = Integer.parseInt(str);
+        System.out.println(i * 2);
+    }
+}
