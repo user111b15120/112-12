1a13e048176cfc4ad984903c8d9ec55f38818401
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-10 16:28:30 +0800

    Create Ex23.java

---
+++ Ex23/Ex23.java
@@ -0,0 +1,9 @@
+package L12.Ex23;
+
+public class Ex23 {
+    public static void main(String[] args){
+        
+        String str = Double.toString(67.834);
+        System.out.println(str.length());
+    }
+}
