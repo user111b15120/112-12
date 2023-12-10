1d6535e75e9f831e777ce3e8cb302865061cb43a
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-10 16:01:50 +0800

    Create Ex15.java

---
+++ Ex15/Ex15.java
@@ -0,0 +1,9 @@
+package L12.Ex15;
+
+public class Ex15 {
+    public static void main(String[] args){
+        double number = 123.45;
+        String string = Double.toString(number);
+        System.out.println("string: " + string.length());
+    }
+}
