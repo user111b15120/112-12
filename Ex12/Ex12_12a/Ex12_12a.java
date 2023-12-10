d0649cc41ac11b9cfc3842d8e0910633f61f4bd4
user111b15120 <130130410+user111b15120@users.noreply.github.com>
2023-12-10 14:17:58 +0800

    Create Ex12_12a.java

---
+++ Ex12/Ex12_12a/Ex12_12a.java
@@ -0,0 +1,14 @@
+package L12.Ex12.Ex12_12a;
+
+class Factor{
+    public int fac(int n){
+        if(n >= 1)
+            return n * fac(n-1);
+        else return 1;
+    }
+}
+
+public class Ex12_12a {
+    Factor f = new Factor();
+    System.out.println("factor(5)=" + f.fac(5));
+}
